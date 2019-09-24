5d571eea478d30543bbd091ac01f8ec12d7b657e
petrenkoVitaliy <vitaliy.ptt@gmail.com>
2019-09-24 03:29:28 +0300

    added one more bug

--- src/api/jira.api.js
+++ src/api/jira.api.js
@@ -1,20 +1,20 @@
 /*global chrome*/
-import { Logger } from 'helpers';
+import { Logger } from "helpers";
 
-const moduleName = 'JIRA_API';
+const moduleName = "JIRA_API";
 
 export class JiraApiClass {
   getTaskNumber = () => {
     return new Promise((resolve, reject) => {
       if (!chrome.tabs) {
-        Logger.log(moduleName, 'cant find chrome.tabs');
+        Logger.log(moduleName, "cant find chrome.tabs");
         resolve(false);
       }
       chrome.tabs.query(
         { active: true, windowId: chrome.windows.WINDOW_ID_CURRENT },
         tabs => {
           const tabUrl = tabs[0].url;
-          const post = tabUrl.indexOf('browse/');
+          const post = tabUrl.indexOf("browse/");
           const taskNumber = tabUrl.slice(post + 7);
 
           Logger.log(moduleName, `founded task number: ${taskNumber}`);
@@ -27,12 +27,12 @@ export class JiraApiClass {
   getTaskTitle = () => {
     return new Promise((resolve, reject) => {
       if (!chrome.tabs) {
-        Logger.log(moduleName, 'cant find chrome.tabs');
+        Logger.log(moduleName, "cant find chrome.tabs");
         resolve(false);
       }
       chrome.tabs.executeScript(
         {
-          code: `document.querySelector("*[data-test-id='issue.views.issue-base.foundation.summary.heading']").innerText`
+          code: `(document.querySelector("*[data-test-id='issue.views.issue-base.foundation.summary.heading']") || document.querySelector('[aria-label="Edit Summary"]').parentElement.children[0].innerText).innerText`
         },
         result => {
           Logger.log(moduleName, `founded task title: ${result}`);
